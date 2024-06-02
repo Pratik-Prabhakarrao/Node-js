ac8e9e2c031003ef62feff7ccf68d561165dbba6
PratikPrabhakarrao <Pratikprabhakarraoatwork@gmail.com>
2024-06-03 00:26:13 +0530

    commit 4

---
+++ Node js basic/app4.js
@@ -0,0 +1,16 @@
+const { log } = require('console');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+rl.question("please enter your name :", (name)=>{
+    console.log("you enterd:" +name);
+    rl.close();
+})
+
+rl.on("close",()=>{
+    console.log("Interface is closed");
+    process.exit(0);
+})
