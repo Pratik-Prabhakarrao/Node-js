24bbed44dde1f98d8be37ae6d362fe48315f1d41
PratikPrabhakarrao <Pratikprabhakarraoatwork@gmail.com>
2024-06-03 05:17:54 +0530

    commit 15

---
+++ Node js basic/app15.js
@@ -0,0 +1,48 @@
+const readline = require("readline");
+const fs = require("fs");
+const http = require("http");
+
+const html = fs.readFileSync("./Templates/index13.html", "utf-8");
+const products = JSON.parse(fs.readFileSync("./Data/products.json", "utf-8"));
+
+// Create server
+
+const server = http.createServer((request, response) => {
+  let path = request.url;
+  if (path === "/" || path.toLowerCase() === "/home") {
+    response.writeHead(200, {
+      "Content-type": "text/html",
+      "my-header": "Hello world",
+    });
+    response.end(html.replace("{{%CONTENT%}}", "You are in home page"));
+  } else if (path.toLowerCase() === "/about") {
+    response.writeHead(200, {
+      "Content-type": "text/html",
+      "my-header": "Hello world",
+    });
+    response.end(html.replace("{{%CONTENT%}}", "You are in about page"));
+  } else if (path.toLowerCase() === "/contact") {
+    response.writeHead(200, {
+      "Content-type": "text/html",
+      "my-header": "Hello world",
+    });
+    response.end(html.replace("{{%CONTENT%}}", "You are in contact page"));
+  } else if (path.toLowerCase() === "/products") {
+    response.writeHead(200, {"Content-type": "appliction/json" });
+    response.end('you arte uub ')
+    console.log(products);
+
+  } else {
+    response.writeHead(404, {
+      "Content-type": "text/html",
+      "my-header": "Hello world",
+    });
+    response.end(html.replace("{{%CONTENT%}}", "error 404 : Page not found "));
+  }
+});
+
+// listne to server
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("server has started");
+});
