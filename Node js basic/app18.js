bac7059c419bcca07f3dd5316579f65fe90ede69
PratikPrabhakarrao <Pratikprabhakarraoatwork@gmail.com>
2024-06-04 02:10:12 +0530

    commit 18

--- Node js basic/app18.js
+++ Node js basic/app18.js
@@ -6,21 +6,25 @@ const url = require('url');
 const html = fs.readFileSync("./Templates/index13.html", "utf-8");
 const products = JSON.parse(fs.readFileSync("./Data/products.json", "utf-8"));
 const productListHtml =  fs.readFileSync('./Templates/productlist.html', 'utf8')
+const productDetailsHtml = fs.readFileSync('./Templates/productdetails.html', 'utf8')
 
-let productHtmlArray = products.map((prod)=>{
-    let output = productListHtml.replace('{{%IMAGE%}}', prod.productImage);// first we are replacing the name of property with the db name  
-    output = output.replace('{{%MODELNAME%}}', prod.name);
-    output = output.replace('{{%MODELNO%}}', prod.modeName);
-    output = output.replace('{{%NAME%}}', prod.modelNumber);
-    output = output.replace('{{%SIZE%}}', prod.size);
-    output = output.replace('{{%CAMERA%}}', prod.camera);
-    output = output.replace('{{%PRICE%}}', prod.price);
-    output = output.replace('{{%COLOR%}}', prod.color);
-    output = output.replace('{{%ID%}}', prod.id);
 
-return output;
+ 
+function replaceHtml(emplate, product){
+    let output = emplate.replace('{{%IMAGE%}}', product.productImage);// first we are replacing the name of property with the db name  
+    output = output.replace('{{%MODELNAME%}}', product.name);
+    output = output.replace('{{%MODELNO%}}', product.modeName);
+    output = output.replace('{{%NAME%}}', product.modelNumber);
+    output = output.replace('{{%SIZE%}}', product.size);
+    output = output.replace('{{%CAMERA%}}', product.camera);
+    output = output.replace('{{%PRICE%}}', product.price);
+    output = output.replace('{{%COLOR%}}', product.color);
+    output = output.replace('{{%ID%}}', product.id);
+    output = output.replace('{{%ROM%}}', product.ROM);
+    output = output.replace('{{%DESC%}}', product.Description);
 
-})
+    return output
+}
 
 // Create server
 
@@ -48,11 +52,16 @@ const server = http.createServer((request, response) => {
     response.end(html.replace("{{%CONTENT%}}", "You are in contact page"));
   } else if (path.toLowerCase() === "/products") {
     if(!query.id){
+       let productHtmlArray =  products.map((prod)=>{
+          return replaceHtml(productListHtml, prod);
+        });
         let productResponseHtml = html.replace('{{%CONTENT%}}', productHtmlArray.join(','));
         response.writeHead(200, {"Content-type": "text/html" });
         response.end(productResponseHtml);
     }else{
-        response.end();
+        let prod = products[query.id];
+        let productDetailsResponseHtml = replaceHtml(productDetailsHtml, prod);
+        response.end(html.replace('{{%CONTENT%}}', productDetailsResponseHtml));
     }
     //console.log(productHtmlArray.join(',')); returned data is array of html hence join
 
